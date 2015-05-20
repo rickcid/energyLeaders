11470472062cc357deb94398a9ee7a16e80c8863
Rick Cid <rick@hispand.com>
2015-05-20 09:17:46 -0600

    Created Views and Controllers for User

--- scripts/controllers/ConsultantCtrl.js
+++ scripts/controllers/ConsultantCtrl.js
@@ -2,11 +2,27 @@ var app = angular.module('energyLeaders');
 
 app.controller('ConsultantCtrl', function($scope, $routeParams, UserService) {
 
-  $scope.consultants = UserService.consultantData;
-  console.log("Scope.Consultants: ", $scope.consultants)
+  var allConsultants = UserService.consultantData;
+  var consultantData = UserService.consultantData[$routeParams.userId]
+  console.log('All Consultants;' , allConsultants);
+  console.log('Consultant: ' , consultantData);
 
-  console.log("UserrouteParams.id:", $routeParams.id);
-  $scope.consultant = UserService.companyData[$routeParams.id];
+
+  var allprofilesData = UserService.profilesData;
+  console.log("All Profiles Data: ", allprofilesData);
+  var profileData = UserService.profilesData[$routeParams.userId];
+  console.log("Profile:" , profileData);
+
+  $scope.consultants = allConsultants;
+  $scope.consultant = consultantData;
+
+  $scope.profiles = allprofilesData;
+  $scope.profile = profileData;
+
+  if ($routeParams.userId) {
+    $scope.allDegrees = UserService.displayAll(profileData.degrees);
+  }
+
 
 
 });
